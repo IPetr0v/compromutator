debf83bbefdcfc87c6625b70080be249d3d00e24
Ivan Petrov <ipetr0v@mail.ru>
2018-02-08 16:50:15 +0300

    Proxy: Add concurrent job queue, fix proxy message

--- src/proxy/Proxy.cpp
+++ src/proxy/Proxy.cpp
@@ -1,5 +1,6 @@
 #include "Proxy.hpp"
 
+#include <cassert>
 #include <string>
 #include <sstream>
 
@@ -28,8 +29,9 @@ void ProxyClient::connection_callback(OFConnection* connection,
 void ProxyClient::message_callback(OFConnection* connection, uint8_t type,
                                    void* data, size_t len)
 {
+    Message message(type, data, len);
     proxy_->addJob(std::make_unique<ProxyMessageJob>(
-        Origin::FROM_CONTROLLER, connection, type, data, len
+        Origin::FROM_CONTROLLER, connection, message
     ));
 }
 
@@ -51,7 +53,6 @@ ProxyServer::~ProxyServer()
 void ProxyServer::connection_callback(OFConnection* connection,
                                       OFConnection::Event type)
 {
-    //proxy_->onSwitchConnection(connection, type);
     proxy_->addJob(std::make_unique<ProxyConnectionJob>(
         Origin::FROM_SWITCH, connection, type
     ));
@@ -60,20 +61,48 @@ void ProxyServer::connection_callback(OFConnection* connection,
 void ProxyServer::message_callback(OFConnection* connection, uint8_t type,
                                    void* data, size_t len)
 {
-    //proxy_->onSwitchMessage(conn, type, data, len);
+    Message message(type, data, len);
     proxy_->addJob(std::make_unique<ProxyMessageJob>(
-        Origin::FROM_SWITCH, connection, type, data, len
+        Origin::FROM_SWITCH, connection, message
     ));
+}
+
+void ConcurrentJobQueue::addJob(std::unique_ptr<ProxyJob>&& job)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(std::move(job));
+    is_available_.notify_one();
+}
+
+void ConcurrentJobQueue::wait()
+{
+    std::unique_lock<std::mutex> wait_lock(mutex_);
+    is_available_.wait(wait_lock);
+}
+
+void ConcurrentJobQueue::runJobs(Proxy* proxy)
+{
+    std::unique_ptr<ProxyJob> current_job;
+    while (not queue_.empty()) {
+        {
+            std::lock_guard<std::mutex> job_lock(mutex_);
+            current_job = std::move(queue_.front());
+            queue_.pop();
+        }
+
+        current_job->run(proxy);
+    }
 }
 
 Proxy::Proxy()
 {
     controller_address_ = "127.0.0.1";
-    controller_port_ = 6653;
+    controller_port_ = 6655;
     proxy_address_ = "127.0.0.1";
-    proxy_port_ = 6655;
+    proxy_port_ = 6653;
 
     auto thread_num = 1;
+    // TODO: if proxy will use handshake then we need to handle different controller and switch versions
     auto settings = fluid_base::OFServerSettings()
         .supported_version(fluid_msg::of10::OFP_VERSION)
         .supported_version(fluid_msg::of13::OFP_VERSION)
@@ -97,24 +126,8 @@ void Proxy::start()
 {
     is_running_ = true;
     while (is_running_) {
-        std::cout << "- Waiting for jobs" << std::endl;
-        {
-            std::unique_lock<std::mutex> wait_lock(job_mutex_);
-            job_available_.wait(wait_lock);
-        }
-
-        // Run jobs
-        std::cout << "- Running jobs" << std::endl;
-        std::unique_ptr<ProxyJob> current_job;
-        while (not job_queue_.empty()) {
-            {
-                std::lock_guard<std::mutex> job_lock(job_mutex_);
-                current_job = std::move(job_queue_.front());
-                job_queue_.pop();
-            }
-
-            current_job->run(this);
-        }
+        job_queue_.wait();
+        job_queue_.runJobs(this);
     }
 }
 
@@ -129,86 +142,147 @@ void Proxy::stop()
 
 void Proxy::addJob(std::unique_ptr<ProxyJob>&& job)
 {
-    std::lock_guard<std::mutex> lock(job_mutex_);
-    job_queue_.push(std::move(job));
-    job_available_.notify_one();
+    job_queue_.addJob(std::move(job));
 }
 
-void Proxy::onControllerConnection(OFConnection* connection,
+void Proxy::onControllerConnection(OFConnection* controller_connection,
                                    OFConnection::Event type)
 {
-    controller_connection_ = connection;
-
     switch (type) {
     case OFConnection::EVENT_STARTED:
-        std::cout << "Controller connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Controller connection id=" << controller_connection->get_id() << " from "
+                  << controller_connection->get_peer_address()
                   << " started" << std::endl;
+        accept_controller_connection(controller_connection);
         break;
     case OFConnection::EVENT_ESTABLISHED:
-        std::cout << "Controller connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Controller connection id=" << controller_connection->get_id() << " from "
+                  << controller_connection->get_peer_address()
                   << " established" << std::endl;
         break;
     case OFConnection::EVENT_FAILED_NEGOTIATION:
-        std::cout << "Controller connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Controller connection id=" << controller_connection->get_id() << " from "
+                  << controller_connection->get_peer_address()
                   << ": failed version negotiation" << std::endl;
         break;
     case OFConnection::EVENT_CLOSED:
-        std::cout << "Controller connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        delete_proxy_connection(controller_connection->get_id());
+        std::cout << "Controller connection id=" << controller_connection->get_id() << " from "
+                  << controller_connection->get_peer_address()
                   << " closed by the user" << std::endl;
         break;
     case OFConnection::EVENT_DEAD:
-        std::cout << "Controller connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        delete_proxy_connection(controller_connection->get_id());
+        std::cout << "Controller connection id=" << controller_connection->get_id() << " from "
+                  << controller_connection->get_peer_address()
                   << " closed due to inactivity" << std::endl;
         break;
     }
 }
 
-void Proxy::onSwitchConnection(OFConnection* connection,
+void Proxy::onSwitchConnection(OFConnection* switch_connection,
                                OFConnection::Event type)
 {
-    switch_connection_ = connection;
-
     switch (type) {
     case OFConnection::EVENT_STARTED:
-        connect_to_controller(connection);
-        std::cout << "Switch connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Switch connection id=" << switch_connection->get_id() << " from "
+                  << switch_connection->get_peer_address()
                   << " started" << std::endl;
+        start_controller_connection(switch_connection);
         break;
     case OFConnection::EVENT_ESTABLISHED:
-        std::cout << "Switch connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Switch connection id=" << switch_connection->get_id() << " from "
+                  << switch_connection->get_peer_address()
                   << " established" << std::endl;
         break;
     case OFConnection::EVENT_FAILED_NEGOTIATION:
-        std::cout << "Switch connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Switch connection id=" << switch_connection->get_id() << " from "
+                  << switch_connection->get_peer_address()
                   << ": failed version negotiation" << std::endl;
         break;
     case OFConnection::EVENT_CLOSED:
-        std::cout << "Switch connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Switch connection id=" << switch_connection->get_id() << " from "
+                  << switch_connection->get_peer_address()
                   << " closed by the user" << std::endl;
+        delete_proxy_connection(switch_connection->get_id());
         break;
     case OFConnection::EVENT_DEAD:
-        std::cout << "Switch connection id=" << connection->get_id() << " from "
-                  << connection->get_peer_address()
+        std::cout << "Switch connection id=" << switch_connection->get_id() << " from "
+                  << switch_connection->get_peer_address()
                   << " closed due to inactivity" << std::endl;
+        delete_proxy_connection(switch_connection->get_id());
         break;
     }
 }
 
-void Proxy::connect_to_controller(OFConnection* connection)
+void Proxy::onControllerMessage(OFConnection* controller_connection,
+                                Message message)
 {
-    // TODO: change this temporary solution
-    std::thread connection_thread([this, connection]() {
-        client_->add_connection(connection->get_id(),
-                                controller_address_, controller_port_);
+    if (message.type == of13::OFPT_HELLO) {
+        //// Do not proxy handshake
+        //return;
+        std::cout << "Proxying HELLO from the controller" << std::endl;
+    }
+    if (message.type == of13::OFPT_FEATURES_REQUEST) {
+        std::cout << "Proxying FEATURES_REQUEST from the controller" << std::endl;
+    }
+
+    auto connection_id = controller_connection->get_id();
+    auto it = connections_.find(connection_id);
+    if (connections_.end() != it) {
+        auto switch_connection = it->second.switch_connection;
+        send(switch_connection, message);
+    }
+    else {
+        std::cerr << "Controller message proxy error: No proxy connection id="
+                  << connection_id << std::endl;
+    }
+}
+
+void Proxy::onSwitchMessage(OFConnection* switch_connection, Message message)
+{
+    if (message.type == of13::OFPT_HELLO) {
+        //// Do not proxy handshake
+        //return;
+        std::cout << "Proxying HELLO from the switch" << std::endl;
+    }
+    if (message.type == of13::OFPT_FEATURES_REPLY) {
+        std::cout << "Proxying FEATURES_REPLY from the switch" << std::endl;
+    }
+
+    auto connection_id = switch_connection->get_id();
+    auto waiting_it = waiting_connections_.find(connection_id);
+    if (waiting_connections_.end() != waiting_it) {
+        // Queue messages
+        auto& message_queue = waiting_it->second.message_queue;
+        message_queue.push(message);
+    }
+    else {
+        auto it = connections_.find(connection_id);
+        if (connections_.end() != it) {
+            auto controller_connection = it->second.controller_connection;
+            send(controller_connection, message);
+
+        }
+        else {
+            std::cerr << "Switch message proxy error: No proxy connection id="
+                      << connection_id << std::endl;
+        }
+    }
+}
+
+void Proxy::start_controller_connection(OFConnection* switch_connection)
+{
+    auto connection_id = switch_connection->get_id();
+    waiting_connections_.emplace(
+        connection_id, WaitingConnection(switch_connection)
+    );
+
+    // TODO: change this temporary solution (create thread in fluid)
+    std::thread connection_thread([this, connection_id, switch_connection]() {
+        client_->add_connection(connection_id,
+                                controller_address_,
+                                controller_port_);
     });
 
     // Init thread name
@@ -220,15 +294,77 @@ void Proxy::connect_to_controller(OFConnection* connection)
     connection_thread.detach();
 }
 
-void Proxy::onControllerMessage(OFConnection* connection, Message message)
+void Proxy::accept_controller_connection(OFConnection* controller_connection)
 {
-    std::cout << "Proxying Message from the controller" << std::endl;
-    switch_connection_->send(message.data, message.len);
+    auto connection_id = controller_connection->get_id();
+    auto it = waiting_connections_.find(connection_id);
+    if (waiting_connections_.end() != it) {
+        auto switch_connection = it->second.connection;
+
+        // Send delayed messages
+        auto& message_queue = it->second.message_queue;
+        while (not message_queue.empty()) {
+            auto& message = message_queue.front();
+            send(controller_connection, message);
+            message_queue.pop();
+        }
+
+        waiting_connections_.erase(it);
+        add_proxy_connection(controller_connection, switch_connection);
+    }
+    else {
+        // Expired connection
+        controller_connection->close();
+    }
 }
 
-void Proxy::onSwitchMessage(OFConnection* connection, Message message)
+void Proxy::add_proxy_connection(OFConnection* controller_connection,
+                                 OFConnection* switch_connection)
 {
-    // TODO: Check if controller connection is not nullptr
-    std::cout << "Proxying Message from the switch" << std::endl;
-    controller_connection_->send(message.data, message.len);
+    assert(controller_connection->get_id() == switch_connection->get_id());
+    auto connection_id = controller_connection->get_id();
+    auto it = connections_.find(connection_id);
+    if (connections_.end() == it) {
+        connections_.emplace(
+            connection_id,
+            ProxyConnection{controller_connection, switch_connection}
+        );
+
+        std::cout << "Proxy connection id=" << switch_connection->get_id()
+                  << " started" << std::endl;
+    }
+    else {
+        std::cerr << "Connection id=" << connection_id
+                  << " is already presented" << std::endl;
+    }
+}
+
+void Proxy::delete_proxy_connection(ConnectionId id)
+{
+    auto waiting_it = waiting_connections_.find(id);
+    auto connection_it = connections_.find(id);
+    // There cannot be same waiting connections and proxy connections
+    assert((waiting_connections_.end() != waiting_it and
+            connections_.end() == connection_it) or
+           (waiting_connections_.end() == waiting_it and
+            connections_.end() != connection_it));
+
+    if (waiting_connections_.end() != waiting_it) {
+        // TODO: delete old connections and their messages
+        waiting_it->second.connection->close();
+        waiting_connections_.erase(waiting_it);
+    }
+    else if (connections_.end() != connection_it) {
+        connection_it->second.controller_connection->close();
+        connection_it->second.switch_connection->close();
+        connections_.erase(connection_it);
+
+        std::cout << "Proxy connection id=" << id
+                  << " closed" << std::endl;
+    }
+}
+
+void Proxy::send(OFConnection* connection, Message message)
+{
+    connection->send(message.data.get(), message.len);
 }
--- src/proxy/Proxy.hpp
+++ src/proxy/Proxy.hpp
@@ -2,6 +2,8 @@
 
 #include <fluid/OFServer.hh>
 #include <fluid/OFClient.hh>
+#include <fluid/base/BaseOFServer.hh>
+#include <fluid/base/BaseOFClient.hh>
 #include <fluid/OFConnection.hh>
 #include <fluid/of10/openflow-10.h>
 #include <fluid/of13/openflow-13.h>
@@ -17,16 +19,14 @@
 
 #include <iostream>
 using namespace fluid_base;
+using namespace fluid_msg;
 
 using ConnectionId = uint32_t;
-using OFConnectionPtr = std::shared_ptr<OFConnection>;
-/*using MessageCallback = std::function<void(OFConnection*,
-                                           OFConnection::Event)>;*/
 
 class Proxy;
 class ProxyJob;
 
-class ProxyClient : public OFClient
+class ProxyClient : public OFClient//, public OFHandler
 {
 public:
     ProxyClient(Proxy* proxy, uint32_t thread_num, OFServerSettings settings);
@@ -42,7 +42,7 @@ private:
 
 };
 
-class ProxyServer : public OFServer
+class ProxyServer : public OFServer//, public OFHandler
 {
 public:
     ProxyServer(Proxy* proxy, const std::string& address, uint32_t port,
@@ -51,7 +51,6 @@ public:
 
 private:
     Proxy* proxy_;
-    ProxyClient* proxy_client_;
 
     void connection_callback(OFConnection* connection,
                              OFConnection::Event type) override;
@@ -60,23 +59,49 @@ private:
 
 };
 
+struct Message
+{
+    Message(uint8_t type, void* data, size_t len):
+        type(type), data(reinterpret_cast<uint8_t*>(data)), len(len) {}
+
+    uint8_t type;
+    std::shared_ptr<uint8_t> data;
+    size_t len;
+};
+
 struct ProxyConnection
 {
     OFConnection* controller_connection;
     OFConnection* switch_connection;
 };
 
-struct Message
+enum class ProxyCommand
 {
-    Message(uint8_t type, void* data, size_t len):
-        type(type), data(data), len(len) {}
-
-    uint8_t type;
-    void* data;
-    size_t len;
+    FORWARD,
+    DROP
 };
 
-// class ProxyController
+struct WaitingConnection
+{
+    explicit WaitingConnection(OFConnection* connection):
+        connection(connection) {}
+    OFConnection* connection;
+    std::queue<Message> message_queue;
+};
+
+class ConcurrentJobQueue
+{
+public:
+    void addJob(std::unique_ptr<ProxyJob>&& job);
+    void wait();
+    void runJobs(Proxy* proxy);
+
+private:
+    std::mutex mutex_;
+    std::condition_variable is_available_;
+    std::queue<std::unique_ptr<ProxyJob>> queue_;
+};
+
 class Proxy
 {
 public:
@@ -87,13 +112,18 @@ public:
 
     void addJob(std::unique_ptr<ProxyJob>&& job);
 
-    void onControllerConnection(OFConnection* connection,
+    void sendToController(ConnectionId id, Message message) {}
+    void sendToSwitch(ConnectionId id, Message message) {}
+
+    void onControllerConnection(OFConnection* controller_connection,
                                 OFConnection::Event type);
-    void onSwitchConnection(OFConnection* connection,
+    void onSwitchConnection(OFConnection* switch_connection,
                             OFConnection::Event type);
 
-    void onControllerMessage(OFConnection* connection, Message message);
-    void onSwitchMessage(OFConnection* connection, Message message);
+    void onControllerMessage(OFConnection* controller_connection,
+                             Message message);
+    void onSwitchMessage(OFConnection* switch_connection,
+                         Message message);
 
 private:
     std::string controller_address_;
@@ -101,24 +131,37 @@ private:
     std::string proxy_address_;
     uint32_t proxy_port_;
 
+    std::atomic_bool is_running_;
+    ConcurrentJobQueue job_queue_;
+
     std::unique_ptr<ProxyClient> client_;
     std::unique_ptr<ProxyServer> server_;
 
-    OFConnection* controller_connection_;
-    OFConnection* switch_connection_;
+    std::map<ConnectionId, ProxyConnection> connections_;
+    std::map<ConnectionId, WaitingConnection> waiting_connections_;
 
-    std::atomic_bool is_running_;
-    std::mutex job_mutex_;
-    std::queue<std::unique_ptr<ProxyJob>> job_queue_;
-    std::condition_variable job_available_;
+    void start_controller_connection(OFConnection* switch_connection);
+    void accept_controller_connection(OFConnection* controller_connection);
 
-    void connect_to_controller(OFConnection* connection);
+    // TODO: make conections with different types to avoid mistakes
+    void add_proxy_connection(OFConnection* controller_connection,
+                              OFConnection* switch_connection);
+    void delete_proxy_connection(ConnectionId id);
+
+    void send(OFConnection* connection, Message message);
 
 };
+
 enum class Origin
 {
     FROM_CONTROLLER,
     FROM_SWITCH
+};
+
+enum class Destination
+{
+    TO_CONTROLLER,
+    TO_SWITCH
 };
 
 class ProxyJob
@@ -139,14 +182,7 @@ class ProxyConnectionJob : public ProxyJob
 public:
     ProxyConnectionJob(Origin origin, OFConnection* connection,
                        OFConnection::Event event_type):
-        ProxyJob(origin, connection), event_type_(event_type) {
-        if (origin == Origin::FROM_CONTROLLER) {
-            std::cout << "New Controller ProxyConnectionJob" << std::endl;
-        }
-        else {
-            std::cout << "New Switch ProxyConnectionJob" << std::endl;
-        }
-    }
+        ProxyJob(origin, connection), event_type_(event_type) {}
 
     void run(Proxy* proxy) override {
         if (Origin::FROM_CONTROLLER == origin_) {
@@ -164,17 +200,8 @@ private:
 class ProxyMessageJob : public ProxyJob
 {
 public:
-    ProxyMessageJob(Origin origin, OFConnection* connection,
-                    uint8_t message_type, void* message, size_t message_len):
-        ProxyJob(origin, connection),
-        message_(message_type, message, message_len) {
-        if (origin == Origin::FROM_CONTROLLER) {
-            std::cout << "New Controller ProxyMessageJob" << std::endl;
-        }
-        else {
-            std::cout << "New Switch ProxyMessageJob" << std::endl;
-        }
-    }
+    ProxyMessageJob(Origin origin, OFConnection* connection, Message message):
+        ProxyJob(origin, connection), message_(std::move(message)) {}
 
     void run(Proxy* proxy) override {
         if (Origin::FROM_CONTROLLER == origin_) {
@@ -188,3 +215,6 @@ public:
 private:
     Message message_;
 };
+
+// TODO: add ProxyScheduler class for everyone to include and use
+// TODO: implement OutgoingMessageJob, and change names to ConnectionJob and IncomingMessageJob
